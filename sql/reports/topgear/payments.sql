fc6706cf3d783a13d86d848ceb28ff7c2ecd373d
Justin Gasper <jmgasper@gmail.com>
2025-10-24 06:10:49 +1100

    Update to add challenge ID to payments response, per Topgear request

--- sql/reports/topgear/payments.sql
+++ sql/reports/topgear/payments.sql
@@ -1,5 +1,6 @@
 SELECT
   p.payment_id,
+  w.external_id as challenge_id,
   p.gross_amount,
   p.net_amount,
   p.total_amount,
@@ -7,6 +8,7 @@ SELECT
   p.billing_account,
   p.created_at
 FROM finance.payment p
+INNER JOIN finance.winnings w on p.winnings_id = w.winning_id
 WHERE p.created_at >= $1::timestamptz
   AND p.created_at <= $2::timestamptz
 ORDER BY p.created_at DESC;
