71f124f9ed507e72d75c0778b492048993056b34
Justin Gasper <jmgasper@gmail.com>
2025-12-11 10:19:24 +1100

    Fix type casting issue

--- sql/reports/sfdc/payments.sql
+++ sql/reports/sfdc/payments.sql
@@ -40,5 +40,5 @@ WHERE
     AND ($8::numeric IS NULL OR p.total_amount >= $8::numeric)
     AND ($9::numeric IS NULL OR p.total_amount <= $9::numeric)
     AND ($10::text[] IS NULL OR c.status::text = ANY($10::text[]))
-    AND ($11::text[] IS NULL OR p.payment_status = ANY($11::text[]))
+    AND ($11::text[] IS NULL OR p.payment_status::text = ANY($11::text[]))
 ORDER BY p.created_at DESC
