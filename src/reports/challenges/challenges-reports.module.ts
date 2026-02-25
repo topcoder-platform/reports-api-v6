9b0d40cb314b1d6841f5b4c73d8f6748c21c636e
Justin Gasper <jmgasper@gmail.com>
2026-02-25 16:49:53 +1100

    Fix for challenge report format issue seen in prod

--- src/reports/challenges/challenges-reports.module.ts
+++ src/reports/challenges/challenges-reports.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { CsvSerializer } from "src/common/csv/csv-serializer";
 import { SqlLoaderService } from "src/common/sql-loader.service";
+import { CsvResponseInterceptor } from "src/common/interceptors/csv-response.interceptor";
 import { ChallengesReportsController } from "./challenges-reports.controller";
 import { ChallengesReportsService } from "./challenges-reports.service";
 
 @Module({
   controllers: [ChallengesReportsController],
-  providers: [ChallengesReportsService, SqlLoaderService],
+  providers: [
+    ChallengesReportsService,
+    SqlLoaderService,
+    CsvSerializer,
+    CsvResponseInterceptor,
+  ],
 })
 export class ChallengesReportsModule {}
